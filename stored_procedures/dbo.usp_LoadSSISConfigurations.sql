de9a14b7f94a2f8024378c9d2c5239d169233829
Bruno Freitas <bf297@ldsbc.edu>
2020-02-29 20:50:01 -0700

    “IT 243 - Installed package management framework”

--- stored_procedures/dbo.usp_LoadSSISConfigurations.sql
+++ stored_procedures/dbo.usp_LoadSSISConfigurations.sql
@@ -79,10 +79,10 @@ Connect strings are loaded with passwords to allow for automation of SSIS ETL ba
     -- 2) Solution Level Configurations
 
 
-    -- 2.1) jc
+    -- 2.1) LDSBC_IT243_bf
 	
     DELETE FROM dbo.[SSIS Configurations]
-     WHERE ConfigurationFilter = 'jc';
+     WHERE ConfigurationFilter = 'LDSBC_IT243_bf';
 	
 
 	-- 2.1.1) v_data_share_root
@@ -93,8 +93,8 @@ Connect strings are loaded with passwords to allow for automation of SSIS ETL ba
                                         , ConfiguredValueType)
     VALUES
           (
-           'jc'
-		 , 'C:\Users\z035330\Documents\JJAUSSI\Other\JC\projects\LDSBC\IT_243\repos\DFNB_dw\txt_files\'
+           'LDSBC_IT243_bf'
+		 , 'C:\Users\brunodifreitas\Desktop\GIT Repository\DFNB_src\txt_files\'
          , '\Package.Variables[User::v_data_share_root].Properties[Value]'
          , 'String'
           );
